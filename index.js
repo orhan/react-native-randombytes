2745d9d32edc9dd268f496e02b247805cfe9e9ed
Orhan Sönmez <orhnsnmz@gmail.com>
2016-03-06 00:42:53 +0100

    Removed SJCL from package

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer
 }
 
-let sjcl = require('sjcl')
 let RNRandomBytes = require('react-native').NativeModules.RNRandomBytes
 
 function noop () {}
@@ -11,39 +10,9 @@ function toBuffer (nativeStr) {
   return new Buffer(nativeStr, 'base64')
 }
 
-function init () {
-  if (RNRandomBytes.seed) {
-    let seedBuffer = toBuffer(RNRandomBytes.seed)
-    addEntropy(seedBuffer)
-  } else {
-    seedSJCL()
-  }
-}
-
-function addEntropy (entropyBuf) {
-  let hexString = entropyBuf.toString('hex')
-  let stanfordSeed = sjcl.codec.hex.toBits(hexString)
-  sjcl.random.addEntropy(stanfordSeed)
-}
-
-export function seedSJCL (cb) {
-  cb = cb || noop
-  randomBytes(4096, function (err, buffer) {
-    if (err) return cb(err)
-
-    addEntropy(buffer)
-  })
-}
-
 export function randomBytes (length, cb) {
   if (!cb) {
-    let size = length
-    let wordCount = Math.ceil(size * 0.25)
-    let randomBytes = sjcl.random.randomWords(wordCount, 10)
-    console.log(randomBytes)
-    let hexString = sjcl.codec.hex.fromBits(randomBytes)
-    hexString = hexString.substr(0, size * 2)
-    return new Buffer(hexString, 'hex')
+    cb("Error: No callback function defined");
   }
 
   RNRandomBytes.randomBytes(length, function(err, base64String) {
@@ -54,5 +23,3 @@ export function randomBytes (length, cb) {
     }
   })
 }
-
-init()
